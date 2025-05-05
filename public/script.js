39e0e3d2c11e7047b3dcb1644e01c787d0b34dbe
Cristine Chvala@gmail.com <cristine.chvala@gmail.com>
2025-05-05 17:38:26 -0400

    added additional quotes

--- public/script.js
+++ public/script.js
@@ -3,7 +3,16 @@ const quotes = [
   "Say less than necessary. – The 48 Laws of Power",
   "Plan all the way to the end. – Law 29",
   "Strike the shepherd and the sheep will scatter. – Law 42",
-  "Assume formlessness. – Law 48"
+  "Assume formlessness. – Law 48", "Small steps every day lead to big results.",
+  "Your future is created by what you do today, not tomorrow. — Robert Kiyosaki",
+  "Don’t watch the clock; do what it does. Keep going. — Sam Levenson",
+  "You don’t have to be great to start, but you have to start to be great. — Zig Ziglar",
+  "Success is the sum of small efforts, repeated day in and day out. — Robert Collier",
+  "The secret of getting ahead is getting started. — Mark Twain",
+  "Discipline is the bridge between goals and accomplishment. — Jim Rohn",
+  "Push yourself, because no one else is going to do it for you.",
+  "Done is better than perfect. — Sheryl Sandberg",
+  "Your only limit is you."
 ];
 
 let currentQuoteIndex = 0;
